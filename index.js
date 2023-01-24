c7aaf3863b37b7cb882e85531a1b2aada9c3854b
mimnets <mdmonirulislammonir@gmail.com>
2023-01-24 14:53:11 +0600

    Basic server configuration done

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express());
+
+app.get('/', async (req, res)=>{
+    res.send('SNS Internet Server is running..');
+});
+
+app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+});
