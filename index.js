985566d945300854b70fabeb50e78f08083fc09f
mimnets <mdmonirulislammonir@gmail.com>
2023-01-24 20:07:31 +0600

    added mongodb uri and client

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const cors = require('cors');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER}@cluster0.ak6zw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+
+        const customersCollection = client.db("snsInternet").collection("customers");
+
+        
+
+
+
+
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error => console.error(error));
+
+
 
 app.get('/', async (req, res)=>{
     res.send('SNS Internet Server is running..');
