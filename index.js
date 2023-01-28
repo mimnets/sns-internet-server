5ae025e7c6e5e963fda236052bca8fa3253f006c
Monirul Islam <mdmonirulislammonir@gmail.com>
2023-01-29 00:26:45 +0600

    update issue still in stuck

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function run() {
         // Add Customer End
 
         // Update the Customer Start
-        app.put('/customer/:id', async (req, res) => {
+        app.put('/customers/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const customer = req.body;
